b5dac1781ace2bd664c8423bee3f7c8d47502e83
Jungyeon Lee <160304167+LeeJungYeonn@users.noreply.github.com>
2024-08-13 16:12:49 +0900

    Title: 중앙값 구하기, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240813/중앙값 구하기/find-the-median.py
+++ 240813/중앙값 구하기/find-the-median.py
@@ -1,35 +1,16 @@
 a, b, c = map(int, input().split())
 
-median = 0
 if a > b:
-    if b > c:
-        median = b
-    if c > a:
-        median = a
-if a > c:
-    if c > b:
-        median = c
-    if b > a:
-        median = a
-if b > a:
-    if a > c:
-        median = a
-    if c > b:
-        median = b
-if b > c:
-    if c > a:
-        median = c
-    if a > b:
-        median = b
-if c > a:
-    if a > b:
-        median = a
-    if b > c:
-        median = c
-if c > b:
-    if b > a:
-        median = b
-    if a > c:
-        median = c
-
-print(median)
+    if c > a: # c > a > b
+        print(a)
+    elif b > c: # a > b > c
+        print(b)
+    else: # a > c > b
+        print(c)
+else:
+    if c > b: # c > b > a
+        print(b)
+    elif a > c: # b > a > c
+        print(a)
+    else: # b > c > a
+        print(c)
