ff8a2944d9d970ee82156d80e5c2cfa1a51db8a2
Jungyeon Lee <160304167+LeeJungYeonn@users.noreply.github.com>
2024-08-14 23:47:49 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240814/두 개의 격자 비교하기/compare-two-grid.py
+++ 240814/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,29 +1,31 @@
+def make_arr(arr, n, m):
+    for _ in range(0,m):
+        row = list(map(int, input().split()))
+        arr. append(row)
+    return arr
+
 n, m = map(int, input().split())
 
 arr1 = []
-for _ in range(m):
-    row = list(map(int, input().split()))
-    arr1. append(row)
+make_arr(arr1, n, m)
 
 arr2 = []
-for _ in range(m):
-    row = list(map(int, input().split()))
-    arr2.append(row)
+make_arr(arr2, n, m)
 
 result = []
-for _ in range(m):
+for _ in range(0,m):
     row = [0]*n
     result.append(row)
 
 for i in range(n):
-    for j in range(m):
+    for j in range(0,m):
         if arr1[i][j] == arr2[i][j]:
             result[i][j] = 0
         else:
             result[i][j] = 1
 
 for i in range(n):
-    for j in range(m):
+    for j in range(0,m):
         if j != (m-1):
             print(result[i][j], end=' ')
         else:
