5899cad1e6a5958ab7e9577d92cce4deb8637574
Jungyeon Lee <160304167+LeeJungYeonn@users.noreply.github.com>
2024-08-20 00:39:40 +0900

    Title: 건너뛰고 별표 출력하기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/건너뛰고 별표 출력하기/skip-and-print-a-star.py
+++ 240820/건너뛰고 별표 출력하기/skip-and-print-a-star.py
@@ -5,7 +5,7 @@ for i in range(1, n+1):
         print('*', end='')
     print('\n')
 
-for i in range(n, 0, -1):
-    for j in range(i-1, 0, -1):
+for i in range(n-1, 0, -1):
+    for j in range(i, 0, -1):
         print('*', end='')
     print('\n')
