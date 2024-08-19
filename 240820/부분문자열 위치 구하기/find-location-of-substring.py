f66c2720f04d9abc597ef6877ac739277db2db2b
Jungyeon Lee <160304167+LeeJungYeonn@users.noreply.github.com>
2024-08-20 00:06:19 +0900

    Title: 부분문자열 위치 구하기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240820/부분문자열 위치 구하기/find-location-of-substring.py
@@ -0,0 +1,18 @@
+input_str = input()
+trg_str = input()
+
+N = len(input_str)
+start = 0
+
+M = len(trg_str)
+
+for i in range(N - M + 1):
+    all_same = True
+    for j in range(M):
+        if input_str[i + j] != trg_str[j]:
+            all_same = False
+    
+    if all_same == True:
+        start = i
+
+print(start)
