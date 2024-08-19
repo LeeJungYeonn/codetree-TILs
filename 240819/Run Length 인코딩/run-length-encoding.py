000deaf77b0b2368fc3f61e752d8148c49869f16
Jungyeon Lee <160304167+LeeJungYeonn@users.noreply.github.com>
2024-08-19 22:56:30 +0900

    Title: Run Length 인코딩, Time: 129ms, Memory: 73MB, Status: Passed - CodeTree

--- 240819/Run Length 인코딩/run-length-encoding.py
+++ 240819/Run Length 인코딩/run-length-encoding.py
@@ -1,5 +1,4 @@
 A = input()
-A += '0'
 
 s = A[0]
 cnt = 1
@@ -12,6 +11,7 @@ for i in range(1, len(A)):
         run_length += (s + str(cnt))
         s = A[i]
         cnt = 1
+run_length += (s + str(cnt)) 
 
 print(len(run_length))
 print(run_length)
