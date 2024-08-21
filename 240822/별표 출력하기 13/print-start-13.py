a8ab4f329359d1124873369b4f14c935f8ba022f
Jungyeon Lee <160304167+LeeJungYeonn@users.noreply.github.com>
2024-08-22 00:29:43 +0900

    Title: 별표 출력하기 13, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240822/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+num1 = n
+num2 = 1
+for i in range(n):
+    if i % 2 == 0:
+        for j in range(num1):
+            print('* ', end='')
+        print()
+        num1 -= 1
+    else:
+        for j in range(num2):
+            print('* ', end='')
+        print()
+        num2 += 1
+
+for i in range(n):
+    if i % 2 != 0:
+        for j in range(num1):
+            print('* ', end='')
+        print()
+        num1 -= 1
+    else:
+        for j in range(num2):
+            print('* ', end='')
+        print()
+        num2 += 1
